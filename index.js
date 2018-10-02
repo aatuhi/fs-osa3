0268ba7eb3387e0f564b7d3a59efcc747fbbc14e
Aatu <aatu95@gmail.com>
2018-10-02 17:01:40 +0300

    heroku

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+morgan.token('data', function getData(request) {
+  return JSON.stringify(request.body)
+})
 
 app.use(bodyParser.json())
+app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms'))
+app.use(cors())
 
 console.log('Hello world')
 
+// 'visible'-attribuutti lis'tyy kyseenalaisen hakutoiminnon implementaation takia
 let persons = [
   {
     name: 'Arto Hellas',
     number: '040-123456',
-    id: 1
+    id: 1,
+    visible: true
   },
   {
     name: 'Martti Tienari',
     number: '040-123456',
-    id: 2
+    id: 2,
+    visible: true
   },
   {
     name: 'Arto Järvinen',
     number: '040-123456',
-    id: 3
+    id: 3,
+    visible: true
   },
   {
     name: 'Lea Kutvonen',
     number: '040-123456',
-    id: 4
+    id: 4,
+    visible: true
   }
 ]
 
@@ -45,7 +58,6 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
-  console.log(person)
   if (person) {
     response.json(person)
   } else {
@@ -55,7 +67,6 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(body.name.length, body.number.length)
 
   if (body.name.length < 1 || body.number.length < 1) {
     console.log('missing name or number')
@@ -69,12 +80,11 @@ app.post('/api/persons', (request, response) => {
   const person = {
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 100000000)
+    id: body.id
   }
+
   persons = persons.concat(person)
   response.json(person)
-
-  console.log(`${person.name} added to persons`)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -85,7 +95,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
